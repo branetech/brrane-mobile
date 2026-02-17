4d723909686478dc253d84b17b1e3416f43a8f4f
Adheke Joy <joy.adheke@sterling.ng>
2026-02-17 13:44:50 +0100

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,16 @@
 // app/_layout.tsx
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useAppInitialization } from '@/hooks/useAppInitialization';
+import { useRouteProtection } from '@/hooks/useRouteProtection';
+import store, { persistor } from '@/redux/store';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { Toaster } from 'sonner-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useAppInitialization } from '@/hooks/useAppInitialization';
-import { useRouteProtection } from '@/hooks/useRouteProtection';
+import 'react-native-reanimated';
 import { Provider } from 'react-redux';
-import store, { persistor } from '@/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Toaster } from 'sonner-native';
 
 function RootLayoutContent() {
   const colorScheme = useColorScheme();
@@ -31,7 +31,7 @@ function RootLayoutContent() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider value={colorScheme === 'dark' ? DefaultTheme : DarkTheme}>
+      <ThemeProvider value={colorScheme === 'light' ? DefaultTheme : DarkTheme}>
         <Stack screenOptions={{ headerShown: false }} />
         <StatusBar hidden={true} />
         <Toaster />
